f5e6ac0e2dedc925f692a1f0979e5c4558e2cfc4
stew3254 <rtstewy@gmail.com>
2021-02-07 11:55:56 -0500

    Add simple binary tree. No help for actions on it because it's meant for specific use cases. Otherwise you'd use an AVL tree

--- tree.h
+++ tree.h
@@ -4,102 +4,51 @@
 #include "stdlib.h"
 #include "stdbool.h"
 
+//Simple binary tree
 typedef struct TreeNode {
   struct TreeNode *left;
   struct TreeNode *right;
   void *e;
-  unsigned int height;
-} avl_tree_node;
+} tree_node;
 
-typedef struct AVLTree {
-  avl_tree_node *root;
-  unsigned int height;
-} avl_tree;
+typedef struct Tree {
+  tree_node *root;
+  unsigned int len;
+} tree;
 
-static inline avl_tree_node *tree_new_node(void* e) {
-  avl_tree_node *n = (avl_tree_node*) malloc(sizeof(avl_tree_node));
+static inline tree_node *tree_new_node(void* e) {
+  tree_node *n = (tree_node*) malloc(sizeof(tree_node));
   n->left = n->right = NULL;
   n->e = e;
-  n->height = 1;
   return n;
 }
 
-static inline avl_tree *tree_new() {
-  avl_tree *t = (avl_tree*) malloc(sizeof(avl_tree));
-  t->root = NULL;
-  t->height = 0;
+static inline tree *tree_new() {
+  tree *t = (tree*) malloc(sizeof(tree));
+  t->root = tree_new_node(NULL);
+  t->len = 0;
   return t;
 }
 
 
-/* Destroy a avl_tree and all elements inside */
-//Used to free nodes in a avl_tree
-void tree_free_subnodes(avl_tree_node *n, bool is_heap);
-//Not exactly meant to be called directly, main logic behind destroying the avl_tree
-static inline void tree_del(avl_tree *t, bool is_heap) {
+/* Destroy a tree and all elements inside */
+//Used to free nodes in a tree
+void tree_free_subnodes(tree_node *n, bool is_heap);
+//Not exactly meant to be called directly, main logic behind destroying the tree
+static inline void tree_del(tree *t, bool is_heap) {
   //Set initial starting node
   tree_free_subnodes(t->root, is_heap);
   //Walk down the left side
   free(t);
 }
 //Will not remove items that are stack allocated
-static inline void tree_del_stack(avl_tree *t) { tree_del(t, false); }
+static inline void tree_del_stack(tree *t) { tree_del(t, false); }
 //Will free elements before removing the nodes and vec
-static inline void tree_del_heap(avl_tree *t) { tree_del(t, true); }
+static inline void tree_del_heap(tree *t) { tree_del(t, true); }
 
-static inline unsigned int tree_node_height(avl_tree_node* n) {
-  return n == NULL ? 0 : n->height;
-};
-
-static inline bool tree_is_leaf(avl_tree_node *n) { return (n->left == NULL && n->right == NULL); }
-static inline bool tree_is_empty(avl_tree *t) { return tree_is_leaf(t->root); }
-
-void tree_free_subnodes(avl_tree_node *n, bool is_heap);
-
-void *tree_max_from(avl_tree_node *node);
-static inline void *tree_max(avl_tree *t) {
-  return tree_max_from(t->root);
-}
-void *tree_min_from(avl_tree_node *node);
-static inline void *tree_min(avl_tree *t) {
-  return tree_min_from(t->root);
-}
-
-/* Tree rotations */
-avl_tree_node *tree_right_rotate(avl_tree *t, avl_tree_node *y);
-avl_tree_node *tree_left_rotate(avl_tree *t, avl_tree_node *x);
-
-// Get Balance factor of node n
-int tree_get_node_balance(avl_tree_node *n);
-
-// Recursive function to tree_insert_from a key in the subtree rooted
-// with node and returns the new root of the subtree.
-avl_tree_node* tree_insert_from(avl_tree *t, avl_tree_node* node, void *e);
-//Simple wrapper for recursive function
-static void tree_insert(avl_tree *t, void *e) {
-  t->root = tree_insert_from(t, t->root, e);
-}
-
-// Recursive function to delete a node with given e
-// from subtree with given node. It returns node of
-// the modified subtree.
-avl_tree_node* tree_remove_from(avl_tree *t, avl_tree_node* node, void *e);
-//Simple wrapper for recursive function
-static void tree_remove(avl_tree *t, void *e) {
-  t->root = tree_remove_from(t, t->root, e);
-}
-
-// A utility function to print preorder traversal of the avl_tree.
-// The function also prints height of every node
-void tree_printr(avl_tree_node *node, unsigned int offset, char *format);
-static inline void tree_print(avl_tree *t, char *format) {
-  //  Print 4 spaces between nodes
-  tree_printr(t->root, t->height*4, format);
-}
-static inline void tree_println(avl_tree *t, char *format) {
-  //  Print 4 spaces between nodes
-  tree_printr(t->root, t->height*4, format);
-  printf("\n");
-}
+// Check to see if the node is a leaf
+static inline bool tree_is_leaf(tree_node *n) { return (n->left == NULL && n->right == NULL); }
+// Check to see if the avl_tree_is_empty
+static inline bool avl_tree_is_empty(tree *t) { return t->root == NULL; }
 
 #endif
